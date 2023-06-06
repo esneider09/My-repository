d4fe5bf18929cd7fa77ac89073910f6cf6f41f1a
esneider09 <88016998+esneider09@users.noreply.github.com>
2023-06-05 21:26:15 -0500

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+
+for(int i=0; i<10; i++)
+cout<<"2*"<<i<<"="<<2*i<<endl;
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+ int num;
+  cout<<"ingresa la tabla que deceas reailiar: ";
+  cin>> num;
+
+  for(int i=1; i<11; i++)
+    {
+      cout<<num<<"*"<<i<<"="<<num*i<<endl;
+    }
+  
+}
